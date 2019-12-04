027f67e6bd4285fe22de28723620f752f0adc508
Francesco Bruno <46680491+FraBrunoSchool@users.noreply.github.com>
2019-12-04 17:50:45 +0100

    commit

---
+++ CODE/ES_02/cmake-build-debug/CMakeFiles/ES_02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ES_02.dir/main.c.obj"
+  "ES_02.exe"
+  "ES_02.exe.manifest"
+  "ES_02.pdb"
+  "libES_02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ES_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
