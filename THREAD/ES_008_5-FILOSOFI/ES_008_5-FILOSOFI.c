37f101271d964be421b334bca1c2495179495170
Francesco Bruno <francesco.bruno@itiscuneo.eu>
2020-04-27 10:33:59 +0200

    ...

--- THREAD/ES_008_5-FILOSOFI/ES_008_5-FILOSOFI.c
+++ THREAD/ES_008_5-FILOSOFI/ES_008_5-FILOSOFI.c
@@ -1,22 +1,22 @@
 /*
-5 filosofi sono a tavola per mangiare
-Per mangiare occorrono 2 forchette
-Alla destra di ciascun filosofo c'è una forchetta.
-Per mangiare il filosofo deve prendere sia la forchetta alla sua destra
-e anche la forchetta alla sua sinistra
-Dopo aver mangiato il filosofo mette a posto le forchette e pensa per un po'
-Poi gli viene ancora fame e cerca di mangiare
-VINCOLI DEL PROBLEMA
-1) Evidentemente non potranno mangiare tutti contemporaneamente ma occorre assicurare
-il massimo grado di parallelismo. Cioè devo permetere che 2 filosofi (non adiacenti) mangino assieme
-utilizzando 4 delle 5 forchette.
-2) Contemporaneamente andrebbe evitato il deadlock: se ciascun filosofo prende la forchetta
-di sinistra e poi si mette in attesa di quella di destra, si bloccano tutti indefinitamente.
-Se si accorgono del blocco e tutti posano assieme la loro forchetta e poi le riprendono assieme
-allora potrebbero non mangiare mai nessuno (starvation).
-Come evitare il deadlock:
-1. Prendere due forchette assieme, solo quando sono libere entrambe
-2. Prendere le forchette in ordine diverso (tutti prendono per prima la forchetta alla destra, uno solo la forchetta a sinistra).
+  5 filosofi sono a tavola per mangiare
+  Per mangiare occorrono 2 forchette
+  Alla destra di ciascun filosofo c'è una forchetta.
+  Per mangiare il filosofo deve prendere sia la forchetta alla sua destra
+  e anche la forchetta alla sua sinistra
+  Dopo aver mangiato il filosofo mette a posto le forchette e pensa per un po'
+  Poi gli viene ancora fame e cerca di mangiare
+  VINCOLI DEL PROBLEMA
+    1) Evidentemente non potranno mangiare tutti contemporaneamente ma occorre assicurare
+      il massimo grado di parallelismo. Cioè devo permetere che 2 filosofi (non adiacenti) mangino assieme
+      utilizzando 4 delle 5 forchette.
+    2) Contemporaneamente andrebbe evitato il deadlock: se ciascun filosofo prende la forchetta
+      di sinistra e poi si mette in attesa di quella di destra, si bloccano tutti indefinitamente.
+  Se si accorgono del blocco e tutti posano assieme la loro forchetta e poi le riprendono assieme
+  allora potrebbero non mangiare mai nessuno (starvation).
+  Come evitare il deadlock:
+    1. Prendere due forchette assieme, solo quando sono libere entrambe
+    2. Prendere le forchette in ordine diverso (tutti prendono per prima la forchetta alla destra, uno solo la forchetta a sinistra).
 */
 
 #include <stdio.h>
@@ -42,21 +42,23 @@ int main(int argc, char const *argv[]) {
 void *filosofo(void *num){
   int *i = num;
   while(1){
+    sleep(rand()%N);
     printf("Il filosofo %d sta pensando\n", *i+1);
-    sleep(1);
+    sleep(rand()%N);
     printf("Il filosofo %d ha fame\n", *i+1);
-    sleep(1);
+    sleep(rand()%N);
     // prendi forchette
     if (sem_trywait(&S[*i])==0) {
       /* code */
-      if (sem_trywait(&S[*i+1])==0) {
+      if (sem_trywait(&S[(*i+1)%N])==0) {
         /* code */
         printf("Il filosofo %d mangia\n", *i+1);
-        sleep(1);
+        sleep(rand()%N);
         sem_post(&S[*i]);
-        sem_post(&S[*i+1]);
+        sem_post(&S[(*i+1)%N]);
         // posa forchette
       }else sem_post(&S[*i]);
-    }else sem_post(&S[*i+1]);
+    }else sem_post(&S[(*i+1)%N]);
+    sleep(rand()%N);
   }
 }
